e8ec4294dea9b1baf8b6908b6670bd01a97f7abc
Will Stanley <whs17a@acu.edu>
2020-10-13 20:19:32 -0500

    First Commit "Let there be code"

---
+++ SwiftDice_WillStanleyTests/SwiftDice_WillStanleyTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftDice_WillStanleyTests.swift
+//  SwiftDice_WillStanleyTests
+//
+//  Created by William Stanley on 10/13/20.
+//
+
+import XCTest
+@testable import SwiftDice_WillStanley
+
+class SwiftDice_WillStanleyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
